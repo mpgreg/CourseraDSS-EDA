d370d93b4a01c3ee638d7510fd7027baa87c7661
mpgreg <milesper@gmail.com>
2015-09-24 01:17:35 +0200

    project2 plot1 update

--- project2/plot1.R
+++ project2/plot1.R
@@ -9,10 +9,54 @@
 ## source over the course of the entire year. The data that you will use for this assignment are for 1999, 2002, 2005, and 2008.
 ##  and https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip
 
-outputFile <- "plot1.png"
-
 ##Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? Using the base plotting system, make a 
 ##plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+
+outputFile <- "plot1.png"
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+trueFileMD5 <- "b5f11f80e171a7148029b7f367b3667d"
+
+##setwd("~/Documents/School/coursera/data science/exploratory data analysis/project2/")
+baseDir <- getwd()
+
+zipFile <- paste(baseDir,"/exdata-data-NEI_data.zip", sep="")
+summaryFile <- paste(baseDir, "/summarySCC_PM25.rds", sep="")
+sccFile <- paste(baseDir, "/Source_Classification_Code.rds", sep="")
+dateDownloaded <- NULL
+
+##If the file doesn't already exist in baseDir download it and check md5
+if(!file.exists(zipFile)) {
+        cat(sprintf("Downloading file %s \n\t to location %s.\n", fileURL, zipFile))
+        download.file(fileURL, destfile=zipFile, method="curl",quiet=TRUE)
+        dateDownloaded <- date()
+} else cat(sprintf("Zipped data file already exists in specified location. \n\t Using File: %s \n",zipFile))
+
+zipFileMD5 <- digest::digest(algo = "md5", file=zipFile)
+if(zipFileMD5 != trueFileMD5) {
+        warning("Downloaded file has changed from original source for this script.  
+                        This script may not work as originally intended.")
+}        
+
+##Unzip the files and load the data frames
+if(!file.exists(summaryFile)) {
+        cat(sprintf("Unzipping files %s \n\t ", summaryFile))
+        unzip(zipFile, files=summaryFile)
+} else {
+        cat(sprintf("Unzipped data file already exists. \n\t Using data file: %s \n for NEI Summary Data.", summaryFile))
+        NEI <- readRDS(summaryFile)
+}
+
+if(!file.exists(sccFile)) {
+        cat(sprintf("Unzipping files %s \n\t ", sccFile))
+        unzip(zipFile, files=sccFile)
+} else {
+        cat(sprintf("Unzipped data file already exists. \n\t Using data file: %s \n for SCC Data.\n", sccFile))
+        SCC <- readRDS(sccFile)
+}
+
+if(anyNA(NEI)) cat(sprintf("WARNING: NA missing values in summary Data.\n"))
+if(anyNA(SCC)) cat(sprintf("WARNING: NA missing values in summary Data.\n"))
+return(NEI)
 
 
 ##Open png file 
@@ -22,59 +66,10 @@ png(filename = outputFile, bg = "transparent")
 ##Create the image
 totalEmissions <- xtabs(NEI$Emissions ~ NEI$year)
 barplot(totalEmissions,  main = "Total Emissions by Year",
-     col="red",
-     ylab="Total Emissions (Tons)",
-     xlab="Year")
+        col="red",
+        ylab="Total Emissions (Tons)",
+        xlab="Year")
 
 ##Save/close the image
 dev.off()
 
-
-
-
-loadData <- function() {
-        
-        fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
-        trueFileMD5 <- "b5f11f80e171a7148029b7f367b3667d"
-        
-        ##setwd("~/Documents/School/coursera/data science/exploratory data analysis/project2/")
-        baseDir <- getwd()
-        
-        zipFile <- paste(baseDir,"/exdata-data-NEI_data.zip", sep="")
-        summaryFile <- paste(baseDir, "/summarySCC_PM25.rds", sep="")
-        sccFile <- paste(baseDir, "/Source_Classification_Code.rds", sep="")
-        dateDownloaded <- NULL
-        
-        ##If the file doesn't already exist in baseDir download it and check md5
-        if(!file.exists(zipFile)) {
-                cat(sprintf("Downloading file %s \n\t to location %s.\n", fileURL, zipFile))
-                download.file(fileURL, destfile=zipFile, method="curl",quiet=TRUE)
-                dateDownloaded <- date()
-        } else cat(sprintf("Zipped data file already exists in specified location. \n\t Using File: %s \n",zipFile))
-        
-        zipFileMD5 <- digest::digest(algo = "md5", file=zipFile)
-        if(zipFileMD5 != trueFileMD5) {
-                warning("Downloaded file has changed from original source for this script.  
-                        This script may not work as originally intended.")
-        }        
-        
-        ##Unzip the files and load the data frames
-        if(!file.exists(summaryFile)) {
-                cat(sprintf("Unzipping files %s \n\t ", summaryFile))
-                unzip(zipFile, files=summaryFile)
-        } else {
-                cat(sprintf("Unzipped data file already exists. \n\t Using data file: %s \n for NEI Summary Data.", summaryFile))
-                NEI <- readRDS(summaryFile)
-        }
-        
-        if(!file.exists(sccFile)) {
-                cat(sprintf("Unzipping files %s \n\t ", sccFile))
-                unzip(zipFile, files=sccFile)
-        } else {
-                cat(sprintf("Unzipped data file already exists. \n\t Using data file: %s \n for SCC Data.", sccFile))
-                SCC <- readRDS(sccFile)
-        }
-        
-        if(anyNA(NEI)) cat(sprintf("WARNING: NA missing values in summary Data."))
-        if(anyNA(SCC)) cat(sprintf("WARNING: NA missing values in summary Data."))
-}
